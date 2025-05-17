5cf920f1acb49cd65f7d91fbeb84191cfbd2f448
nechami-v <n0556745397@gmail.com>
2025-05-18 02:21:49 +0300

    client changes

--- Client/Time.js
+++ Client/Time.js
@@ -8,3 +8,19 @@ eventSource.onerror = function(event) {
     console.error("Error:", event);
 };
 
+var wish = document.getElementById("wish")
+function getWish() {
+    var date = new Date();
+    var hour = date.getHours();
+    if (hour < 12&& hour >= 7) {
+        wish.innerText = "Good Morning";
+    } else if (hour < 18&& hour >= 12) {
+        wish.innerText = "Good Afternoon";
+    } else if (hour < 7|| hour >= 22) {
+        wish.innerText = "Good Night";
+    } else {
+
+        wish.innerText = "Good Evening";
+    }
+}
+getWish();
--- WebApplication1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec618413a8ce00db05c20b2bb59627a10eb4686c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
